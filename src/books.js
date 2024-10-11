5338d753c3d0e1834c4fa163486b57ac1c7b1d39
Anthony Nguyen <anthonybnguyen56@gmail.com>
2024-10-11 14:00:24 -0500

    Created a dummy site using react for the first time!

---
+++ src/books.js
@@ -0,0 +1,22 @@
+import img3 from "./images/melania.jpg";
+
+export const books = [
+  {
+    author: "Lisa Marie Presley",
+    title: "From Here to the Great Unknown",
+    img: "https://m.media-amazon.com/images/I/81wThrp1YyL._SY342_.jpg",
+    id: 1,
+  },
+  {
+    author: "Freida McFadden",
+    title: "The Housemaid",
+    img: "https://m.media-amazon.com/images/I/81AHTyq2wVL._SY342_.jpg",
+    id: 2,
+  },
+  {
+    author: "Melania Trump",
+    title: "Melania",
+    img: img3,
+    id: 3,
+  },
+];
